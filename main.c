c21583e103fb6ba30db2572ff92a02bcefaf9991
Tushar4825 <tusharamalalia@gmail.com>
2024-08-14 09:39:36 +0530

    added the tokeniser

--- main.c
+++ main.c
@@ -3,18 +3,23 @@
 #include<string.h>
 #include<limits.h>
 
+#define BUFF_TOKEN_LEN 64
 #define BUFFER_SIZE 1024
+#define TOK_ESC " \n\r\a\t" //these are the escape sequneces
 
-void main_loop(){
+void main_loop(void){
+
     char *line;
     char **args;
-    int binary_result;
+    int binary_result = 1;
 
     do{
+        printf("< ");  
         line = read_line(); //the input to the shell
         args = split_line(line); //gets all the attributes by splittinf the input to the command line
         //the following line updates the status of the shell until exit called  
-        binary_result =  runner(args);
+        binary_result= 1; //testing the loop
+        // binary_result =  runner(args);
         free(line);
         free (args);
 
@@ -26,15 +31,74 @@ char* read_line(){
     int buff_size = BUFFER_SIZE;
     int position = 0;
     char* buffer = malloc(sizeof(char)* buff_size);
+    char holder;
 
     if(!buffer){
-        printf("allocation error for inputs \n");
-        exit(1);
+        fprintf(stderr ,"allocation error for inputs \n");
+        exit(EXIT_FAILURE);
+    }
+    //now we need to read the line until the we reach EOF or an escape sequence
+    while(1){
+
+        holder = getchar();
+        if(holder == EOF || holder == '\n'){
+            buffer[position] = '\0';
+            return buffer;
+        } else{
+            buffer[position] = holder;
+        }  position++;
+
+        if(position >= buff_size){
+
+            buff_size += BUFFER_SIZE;
+            buffer = realloc(buffer, buff_size);
+            if(!buffer){
+                fprintf(stderr,"allocation error \n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
+//we will use the string tokeniser (strtok) to tokenise the stdin line
+char **split_line(char *line){
+
+    int buff_size = BUFF_TOKEN_LEN;
+    int position = 0;
+
+    // this desscribes one token 
+    char **tokens = malloc(buff_size * sizeof(char));
+    //this is the pointer tokens
+    char *token;
+
+    if(!tokens){
+        printf('allocation error !!');
+        exit(EXIT_FAILURE);
+    }
+    //defineing the individual tokens
+    token = strtok(line, TOK_ESC);
+
+    while(token != NULL){
+        tokens[position]=  token;
+        position++;
+
+        //edge cases
+        //here we reallocate
+        if(position> buff_size){
+            buff_size += BUFFER_SIZE;
+            buff_size = realloc(buff_size, sizeof(char*)*buff_size);
+            if(!tokens){
+                printf('realloc error !!');
+                exit(EXIT_FAILURE);
+            }
+        }
+        //now we can retokenise
+        token = strtok(line, TOK_ESC);
     }
 
-    //now we need to read the line until the we reach EOF or an escape sequence
-    
-
+    // add a null pointer the the EOF
+    tokens[position] = NULL;
+    return tokens; //return the tokens array
 }
 
 int main(int argc, char **argv){
@@ -44,3 +108,4 @@ int main(int argc, char **argv){
     return EXIT_SUCCESS;
 
 }
+
